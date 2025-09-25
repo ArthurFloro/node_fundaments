ae45ce08041064a49df745e8d93250183639710e
ArthurFloro <arthur.floro2006@gmail.com>
2025-09-24 21:36:50 -0300

    adiçaõ de regex para parametrizar as rotas da aplicação, adição do método delete no databse

--- src/server.js
+++ src/server.js
@@ -4,16 +4,25 @@ import { routes } from "./routes.js";
 
 // uuid -> UNIVERSAL UNIQUE ID
 
+// O front end tem 3 formas de enviar requisições ao backend
+// Query parameters: URL Stateful => Filtros, paginação, não obrigatórios
+// Route parameters: identificação de recurso
+// Request body: envio de informações em um formulário
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+
+    req.params = { ...routeParams.groups };
+
     return route.handler(req, res);
   }
 
